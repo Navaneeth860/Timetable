b1dcd410b27da7a727a5f51dff51396430a08970
felix <web.navaneeth@gmail.com>
2026-09-08 23:52:18 +0530

    Customized for BMSCE

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "timetable-b-v2";
+const CACHE = "timetable-app-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
