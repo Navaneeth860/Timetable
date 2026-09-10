26cd3e331fcf394c1dbeab1267abad2c7aee9246
felix <web.navaneeth@gmail.com>
2026-09-10 22:50:15 +0530

    Added CSE(Iot) and minor tweaks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "timetable-app-v5";
+const CACHE = "timetable-app-v6";
 const ASSETS = ["./", "./index.html", "./style.css", "./data.json", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
