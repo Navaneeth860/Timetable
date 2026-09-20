c0e4fde2292db182532e83b6ec8c01c4aba0cbc9
felix <web.navaneeth@gmail.com>
2026-09-21 00:38:36 +0530

    Added AIML

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "timetable-app-v7";
+const CACHE = "timetable-app-v10";
 const ASSETS = ["./", "./index.html", "./style.css", "./data.json", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
