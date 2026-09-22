3a4d38c275c43dd0c0c2c0a7cefab91c21af1048
felix <web.navaneeth@gmail.com>
2026-09-22 20:49:14 +0530

    UI tweaking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "timetable-app-v10";
+const CACHE = "timetable-app-v13";
 const ASSETS = ["./", "./index.html", "./style.css", "./data.json", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
